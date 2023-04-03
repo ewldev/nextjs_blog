c951aec768427c73a8eea9f54fa08335d5867ab4
ewldev <edwin.leung2@gmail.com>
2023-04-02 23:48:31 -0400

    generate html from markdown, installed remark remark-html

---
+++ app/posts/[postId]/page.tsx
@@ -0,0 +1,34 @@
+import getFormattedDate from "@/lib/getFormattedDate"
+import { notFound } from "next/navigation"
+
+export function generateMetadata({ params }: { params: { postId: string } }) {
+
+    const posts = getSortedPostsData() //deduped!
+    const { postId } = params
+
+    const post = posts.find(post => post.id === postId)
+
+    if (!post) {
+        return {
+            title: 'Post Not Found'
+        }
+    }
+
+    return {
+        title: post.title,
+    }
+}
+
+export default async function Post({ params }: { params: { postId: string } }) {
+
+    const posts = getSortedPostsData() //deduped!
+    const { postId } = params
+
+    if (!posts.find(post => post.id === postId)) {
+        return notFound()
+    }
+
+    return (
+        <div>page</div>
+    )
+}
